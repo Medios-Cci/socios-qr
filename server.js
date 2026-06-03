76418abb690d721572fbece6d81bdf56f1cb3dfd
Medios-Cci <medios@cciargentina.org.ar>
2026-06-03 13:43:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,12 @@ const json = JSON.parse(text.substring(start, end + 1));
     const filas = json.table.rows;
 
     // Buscamos el socio por token
+    console.log('Token buscado::' , token);
+    console.log('Primera fila:', JSON.stringify'[0]);
     const socio = filas.find(fila => {
       const valorToken = fila.c[5]?.v;
+      console.log('Comparando:', valorToken, 'con', token);
+      return String(valorToken) === String(token);
 
     });
 
